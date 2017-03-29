42a6fab8b3a35b6b25ed454bba0d0136c586867d
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2017-03-29 17:59:14 -0300

    Creating the project

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack           = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var PROD = JSON.parse(process.env.PROD_ENV || '0');
+
+var htmlConf = new HtmlWebpackPlugin({
+      title: 'Vue App',
+      template: 'src/index.ejs', // Load a custom template (ejs by default see the FAQ for details) 
+      filename: 'dist/index.html'
+    });
+
+module.exports = [{
+  name: 'Bundle JS',
+  entry: ['./src/index.js'],
+  output: {
+    filename: PROD ? './dist/bundle.min.js' : './dist/bundle.js'
+  },
+  plugins: PROD ? [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    }),
+    htmlConf
+  ] : [
+    htmlConf
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      }
+    ]
+  },
+  resolve: {
+      alias: { 'vue' : '../node_modules/vue/dist/vue.min.js'}
+  }
+}];
+
+  // entry: ['./src/index.js', './src/scss/main.scss'],
+    // {
+    //   test: /\.scss$/,
+    //   use: [{
+    //       loader: "style-loader" // creates style nodes from JS strings
+    //   }, {
+    //       loader: "css-loader" // translates CSS into CommonJS
+    //   }, {
+    //       loader: "sass-loader" // compiles Sass to CSS
+    //   }]
+    // },
